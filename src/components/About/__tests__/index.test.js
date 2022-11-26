9004baceefc5dc58db55bb9841474f884c374f66
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-11-25 19:10:23 -0600

    added all tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-
-import About from "..";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
 
-describe("About component", ()=> {
-    //renders About tests
-    it("renders", ()=> {
-        render(<About />);
-    });
-    it("matches snapshot DOM node structure", ()=> {
-        const { asFragment } = render(<About/>);
-        expect(asFragment()).toMatchSnapshot();
-    })
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
